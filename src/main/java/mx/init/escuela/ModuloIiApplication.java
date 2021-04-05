755d83ec390eb496267eaef8c6e0e811a3cc71c8
Guillermo Chang <guillermo.chang@hotmail.com>
2021-04-04 20:37:13 -0500

    CHORE: Se crea el proyecto en Spring Tool Suite 4

---
+++ src/main/java/mx/init/escuela/ModuloIiApplication.java
@@ -0,0 +1,13 @@
+package mx.init.escuela;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModuloIiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModuloIiApplication.class, args);
+	}
+
+}
